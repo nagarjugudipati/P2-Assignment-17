1b6670e0b13885a18ce7de8cf8af94e5fa5725fc
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-09-04 15:00:33 +0530

    P2-Assignment17

---
+++ Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAssignment17.Model;
+
+namespace WebAPIAssignment17.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIAssignment17.Model.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Model/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPIAssignment17.Model
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+    }
+}
